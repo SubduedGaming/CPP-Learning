cf072b578f97439b74f0716efb6a2d9b4d1af480
SubduedGaming <76713813+SubduedGaming@users.noreply.github.com>
2021-01-02 15:57:34 +0000

    update

--- first.cpp
+++ first.cpp
@@ -3,12 +3,13 @@
 using std::cout;
 using std::cin;
 
+
 int main()
 {
-	int slices; //defining variable
+	int slices; //declare variable
 	int eaten;
 	cout << "how many slices? \n ";
-	cin >> slices; //asigning to the variable
+	cin >> slices; //define variable
 	cout << "How many slices eaten? \n";
 	cin >> eaten;
 	int left = slices - eaten;
